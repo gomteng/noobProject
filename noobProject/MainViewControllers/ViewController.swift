804af4833b81ba5ac694b5b58638012e06d29da1
sangjin-lee <sangjingomteng@gmail.com>
2020-11-13 10:29:59 +0900

    tableView

--- noobProject/MainViewControllers/ViewController.swift
+++ noobProject/MainViewControllers/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
+    //tableView 사용을 위한 객체들
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petit e Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli" , "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donos tia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyst er", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "p alominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleav es", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitc hen"]
+    
+//    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Ho ng Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "Lon
+//    don", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causu al Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood" , "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "L atin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
