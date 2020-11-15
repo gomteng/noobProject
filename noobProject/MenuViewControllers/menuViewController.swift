cd945cb9b1251dca173fa1dd13f8031886017c44
sangjin-lee <sangjingomteng@gmail.com>
2020-11-15 13:08:55 +0900

    tableview-error

--- noobProject/MenuViewControllers/menuViewController.swift
+++ noobProject/MenuViewControllers/menuViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class menuViewController: UIViewController {
+    
+    
+    //tableView 사용을 위한 객체들
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petit e Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli" , "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donos tia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyst er", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "p alominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleav es", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitc hen"]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causu al Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood" , "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "L atin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
